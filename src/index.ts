91125eccbaa3b2b3abdc6c215cf2f4165b6a2538
nellfs <pokycelit@gmail.com>
2022-12-20 17:15:49 -0300

    init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+import express, { Express, Request, Response } from "express";
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.listen(3000);
