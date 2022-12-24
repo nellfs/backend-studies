6440da489f40f2b6a2512748c13053985a53db53
nellfs <pokycelit@gmail.com>
2022-12-24 17:38:25 -0300

    🪦  removing nodemon

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -23,6 +23,6 @@ app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333!');
+app.listen(4444, () => {
+  console.log('Server started on port 4444!');
 });
