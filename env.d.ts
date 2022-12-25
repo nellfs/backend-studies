09eaa65e326d7e6d794bcdbcb9a930be00ff851f
nellfs <pokycelit@gmail.com>
2022-12-25 19:45:05 -0300

    env

---
+++ env.d.ts
@@ -0,0 +1,13 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DB_HOST: string;
+      DB_PORT: number;
+      DB_USERNAME: string;
+      DB_PASSWORD: string;
+      DB_NAME: string;
+    }
+  }
+}
+
+export {};
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,9 +1,10 @@
+import 'dotenv/config';
 import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
-  host: 'localhost',
   type: 'postgres',
-  port: 5432,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   username: 'postgres',
   password: 'docker',
   database: 'apivendas',
