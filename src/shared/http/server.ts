7936ad9eaaa3f020d513894d22ed6a20838ae229
nellfs <pokycelit@gmail.com>
2022-12-30 23:25:25 -0300

    refactor: basic refactor

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,11 @@
 import 'express-async-errors';
 import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 
 import '../typeorm';
 import { errorMiddleware } from 'src/middlewares/error';
-import { AppDataSource } from 'src/data-source';
 
 const app = express();
 
